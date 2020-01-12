78c8402045b1e916a446034d330dd288737768fb
PoststresSQL <Adamwage@hotmail.com>
2020-01-12 17:47:58 -0600

    Inital state set, passing props to child components.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,56 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  // Set initial state of sushi to be an empty array
+  constructor() {
+    super();
+    this.state = {
+      allSushi: [],
+      budget: 200,
+      tableSushi: [],
+    };
+  }
+
+  addToTableSushi = (eatenSushi) => {
+    // set a variable to hold the new value of tableSushi because state can't be pushed on
+    const newTableSushi = this.state.tableSushi
+    // push the  eatenSushi into the newTableSushi variable
+    newTableSushi.push(eatenSushi)
+    // spread the previous state so older keys aren't deleted
+    // set the updated key `tableSushi` to the newTableSushi value
+    this.setState({
+      ...this.state,
+      tableSushi: newTableSushi
+    })
+  }
+
+  // create a fetch request to the sushi API
+  componentDidMount() {
+    // this lifecycle method gets run after the render()
+    fetch(API)
+      .then(response => response.json())
+      .then((data) => this.setState({ allSushi: data }));
+  }
 
   render() {
+    const {
+      allSushi,
+      budget,
+      tableSushi
+    } = this.state
+
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer
+          addToTableSushi={this.addToTableSushi}
+          budget={budget}
+          sushiArr={allSushi}
+          tableSushi={tableSushi}
+        />
+        <Table
+          budget={budget}
+          tableSushi={tableSushi}
+        />
       </div>
     );
   }
