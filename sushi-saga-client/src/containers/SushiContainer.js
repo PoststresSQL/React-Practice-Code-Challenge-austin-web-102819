e72273bfeda9bf193e018187fa7587bcd84a0407
PoststresSQL <Adamwage@hotmail.com>
2020-01-12 17:50:25 -0600

    displaying list of sushi at increments of 4

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,33 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
+import Sushi from '../components/Sushi'
 import MoreButton from '../components/MoreButton'
 
-const SushiContainer = (props) => {
+const SushiContainer = ({ addToTableSushi, budget, sushiArr, tableSushi }) => {
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(4);
+
   return (
     <Fragment>
       <div className="belt">
         {
-          /* 
-             Render Sushi components here!
-          */
+          // map through all the sushi, assign a property to each individual sushi
+          // props.sushiArr.map((sushi) => <Sushi key={`${sushi.id}-${sushi.name}`} sushi={sushi} />)
+          sushiArr
+            .slice(startIndex, endIndex)
+            .map((sushi) => (
+              <Sushi
+                addToTableSushi={addToTableSushi}
+                budget={budget}
+                key={`${sushi.id}-${sushi.name}`}
+                sushi={sushi}
+                tableSushi={tableSushi}
+              />
+            ))
         }
-        <MoreButton />
+        <MoreButton
+          indices={{ startIndex, endIndex }}
+          updaters={{ setStartIndex, setEndIndex }}
+        />
       </div>
     </Fragment>
   )
