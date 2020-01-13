7e2a71ee4e18dec8a5b3a49791127072de1f47e6
PoststresSQL <Adamwage@hotmail.com>
2020-01-12 18:16:38 -0600

    Added props for child component logics

--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -5,13 +5,13 @@ import MoreButton from '../components/MoreButton'
 const SushiContainer = ({ addToTableSushi, budget, sushiArr, tableSushi }) => {
   const [startIndex, setStartIndex] = useState(0);
   const [endIndex, setEndIndex] = useState(4);
+  const sushiArrLength = sushiArr.length // 100
 
   return (
     <Fragment>
       <div className="belt">
         {
           // map through all the sushi, assign a property to each individual sushi
-          // props.sushiArr.map((sushi) => <Sushi key={`${sushi.id}-${sushi.name}`} sushi={sushi} />)
           sushiArr
             .slice(startIndex, endIndex)
             .map((sushi) => (
@@ -20,12 +20,14 @@ const SushiContainer = ({ addToTableSushi, budget, sushiArr, tableSushi }) => {
                 budget={budget}
                 key={`${sushi.id}-${sushi.name}`}
                 sushi={sushi}
+                sushiArr={sushiArr}
                 tableSushi={tableSushi}
               />
             ))
         }
         <MoreButton
           indices={{ startIndex, endIndex }}
+          sushiArrLength={sushiArrLength}
           updaters={{ setStartIndex, setEndIndex }}
         />
       </div>
