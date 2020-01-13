a75a210a4b2614a96492d57d21afa66087ce26fd
PoststresSQL <Adamwage@hotmail.com>
2020-01-12 18:15:26 -0600

    removing eaten sushi from allSushi

--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -9,18 +9,21 @@ const Sushi = ({
     name,
     price,
   },
+  sushiArr,
   tableSushi
 }) => {
   const [eaten, setEaten] = useState(false)
   const totalCost = tableSushi.reduce((total, sushi) => (total += sushi.price), 0)
 
-
   return (
     <div className="sushi">
       <div className="plate"
         onClick={() => {
+          const sushiIndex = sushiArr.indexOf(sushi)
           if (budget > (totalCost + price)) {
+            // splice starting at index, end at 1
             setEaten(true)
+            sushiArr.splice(sushiIndex, 1)
             addToTableSushi(sushi)
           } else {
             alert('broke ass nigga alert')
